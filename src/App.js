993fe06a961a22eb35cdb7307a020b5bd0ac17a0
AleAmg <alexisgiozza@gmail.com>
2023-03-16 18:25:04 -0300

    add table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table">
+        <tr>
+          <th>-</th>
+          <th>9:00</th>
+          <th>10:00</th>
+          <th>11:00</th>
+          <th>12:00</th>
+          <th>13:00</th>
+          <th>14:00</th>
+          <th>15:00</th>
+          <th>16:00</th>
+          <th>17:00</th>
+          <th>18:00</th>
+          <th>19:00</th>
+          <th>20:00</th>
+          <th>21:00</th>
+        </tr>
+        <tr>
+          <th>LUNES</th>
+          <td colSpan={5}></td>
+          <td colSpan={3}>Matematica discreta</td>
+          <td></td>
+          <td colSpan={4}>Analisis y produccion del discurso</td>
+        </tr>
+        <tr>
+          <th>MARTES</th>
+        </tr>
+        <tr>
+          <th>MIERCOLES</th>
+        </tr>
+        <tr>
+          <th>JUEVES</th>
+        </tr>
+        <tr>
+          <th>VIERNES</th>
+        </tr>
+        <tr>
+          <th>SABADO</th>
+        </tr>
+      </table>
     </div>
   );
 }
